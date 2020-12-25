53fdd609d139667fda5571928329e933c7812140
ozgur <ozgurturan0@gmail.com>
2020-12-25 13:53:26 +0300

    commit

--- src/test/java/pages/US_009AddHotelRoom.java
+++ src/test/java/pages/US_009AddHotelRoom.java
@@ -1,2 +1,11 @@
-package pages;public class US_009AddHotelRoom {
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US_009AddHotelRoom {
+
+    public US_009AddHotelRoom(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 }
