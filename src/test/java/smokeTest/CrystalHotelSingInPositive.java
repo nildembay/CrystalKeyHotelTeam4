2e356ea350161fa99e41ae6667048b6535778e1c
nildembay <74560545+nildembay@users.noreply.github.com>
2020-12-26 18:55:23 +0300

    Update CrystalHotelSingInPositive.java

--- src/test/java/smokeTest/CrystalHotelSingInPositive.java
+++ src/test/java/smokeTest/CrystalHotelSingInPositive.java
@@ -1,13 +1,14 @@
 package smokeTest;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.CrystalHotelSignIn;
 import utilities.ConfigReader;
 import utilities.Driver;
+import org.testng.Assert;
 
 public class CrystalHotelSingInPositive {
-
     @Test
     public void SignIn(){
         Driver.getDriver().get(ConfigReader.getProperty("p_url"));
@@ -16,7 +17,7 @@ public class CrystalHotelSingInPositive {
         crystalHotelSignIn.userNameTextBox.sendKeys(ConfigReader.getProperty("valid_user"));
         crystalHotelSignIn.passwordTextBox.sendKeys(ConfigReader.getProperty("valid_password"));
         crystalHotelSignIn.girisButton.click();
-
+        Assert.assertTrue(crystalHotelSignIn.managerButton.isDisplayed());
 
     }
 
