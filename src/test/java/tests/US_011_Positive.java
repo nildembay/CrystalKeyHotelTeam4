25157ed9daa1606105213bacafbaf8e2803b66ad
Murat <murat.ozben1223@gmail.com>
2020-12-27 03:21:50 +0100

    birincicommit

--- src/test/java/tests/US_011_Positive.java
+++ src/test/java/tests/US_011_Positive.java
@@ -1,32 +1,96 @@
 package tests;
 
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.US_011_HotelRoomSearchPositivePage;
+import pages.US_011_012_HotelRoomSearchPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class US_011_Positive {
 
-    @Test
-    public void positiveTest1() throws InterruptedException {
+    US_011_012_HotelRoomSearchPage us_011_hotelRoomSearchPositivePage = new US_011_012_HotelRoomSearchPage();
+
+    public void login (){
         Driver.getDriver().get(ConfigReader.getProperty("p_url"));
-        US_011_HotelRoomSearchPositivePage us_011_hotelRoomSearchPositivePage = new US_011_HotelRoomSearchPositivePage();
+
         us_011_hotelRoomSearchPositivePage.loginButonu.click();
         us_011_hotelRoomSearchPositivePage.userNameTextBox.sendKeys(ConfigReader.getProperty("valid_user"));
         us_011_hotelRoomSearchPositivePage.passwordTextBox.sendKeys(ConfigReader.getProperty("valid_password"));
         us_011_hotelRoomSearchPositivePage.girisButtonu.click();
         us_011_hotelRoomSearchPositivePage.hotelManagementLinki.click();
         us_011_hotelRoomSearchPositivePage.hotelRoomsLinki.click();
+    }
+    @Test
+    public void tc_001 () throws InterruptedException {
 
+        login();
         Thread.sleep(5000);
 
         Select select = new Select(us_011_hotelRoomSearchPositivePage.hotelIDDropdown);
-        select.selectByVisibleText(ConfigReader.getProperty("ck_IDHotel_name"));
+        select.selectByVisibleText(ConfigReader.getProperty("tc1_IDHotel_name"));
 
         us_011_hotelRoomSearchPositivePage.searchButton.click();
+        Assert.assertTrue(us_011_hotelRoomSearchPositivePage.sonucYazisi.isDisplayed());
 
+
+    }
+    @Test
+    public void tc_002(){
+        login();
+        Select select = new Select(us_011_hotelRoomSearchPositivePage.groupRoomTypeDropdown);
+        select.selectByValue("318");
+        us_011_hotelRoomSearchPositivePage.searchButton.click();
+        Assert.assertTrue(us_011_hotelRoomSearchPositivePage.sonucYazisi.isDisplayed());
+
+
+    }
+    @Test
+    public void tc_003(){
+        login();
+
+        us_011_hotelRoomSearchPositivePage.idTextBox.sendKeys(ConfigReader.getProperty("tc3and10_ID_textbox"));
+        us_011_hotelRoomSearchPositivePage.codeTextBox.sendKeys(ConfigReader.getProperty("tc3_code_textbox"));
+        us_011_hotelRoomSearchPositivePage.nameTextBox.sendKeys(ConfigReader.getProperty("tc3_name_textbox"));
+        us_011_hotelRoomSearchPositivePage.locationTextBox.sendKeys(ConfigReader.getProperty("tc3_location_textbox"));
+        us_011_hotelRoomSearchPositivePage.priceTextBox.sendKeys(ConfigReader.getProperty("tc3and10_price_textbox"));
+        Select select = new Select(us_011_hotelRoomSearchPositivePage.isAvalaibleDropdown);
+        select.selectByVisibleText("True");
+        us_011_hotelRoomSearchPositivePage.searchButton.click();
+        Assert.assertTrue(us_011_hotelRoomSearchPositivePage.sonucYazisi.isDisplayed());
+
+    }
+    @Test
+    public void tc_004(){
+        login();
+        us_011_hotelRoomSearchPositivePage.idTextBox.sendKeys(ConfigReader.getProperty("tc4and5_ID_textbox"));
+        Select select = new Select(us_011_hotelRoomSearchPositivePage.hotelIDDropdown);
+        select.selectByVisibleText(ConfigReader.getProperty("tc4and5_IDHotel_name"));
+        us_011_hotelRoomSearchPositivePage.codeTextBox.sendKeys(ConfigReader.getProperty("tc4and5_code_textbox"));
+        us_011_hotelRoomSearchPositivePage.nameTextBox.sendKeys(ConfigReader.getProperty("tc4and5_name_textbox"));
+        us_011_hotelRoomSearchPositivePage.locationTextBox.sendKeys(ConfigReader.getProperty("tc4and5_location_textbox"));
+        us_011_hotelRoomSearchPositivePage.priceTextBox.sendKeys(ConfigReader.getProperty("tc4and5_price_textbox"));
+        Select select1 = new Select(us_011_hotelRoomSearchPositivePage.isAvalaibleDropdown);
+        select1.selectByVisibleText("True");
+        us_011_hotelRoomSearchPositivePage.searchButton.click();
+        Assert.assertTrue(us_011_hotelRoomSearchPositivePage.sonucYazisi.isDisplayed());
+
+    }
+    @Test
+    public void tc_005(){
+        login();
+        us_011_hotelRoomSearchPositivePage.idTextBox.sendKeys(ConfigReader.getProperty("tc4and5_ID_textbox"));
+        Select select = new Select(us_011_hotelRoomSearchPositivePage.hotelIDDropdown);
+        select.selectByVisibleText(ConfigReader.getProperty("tc4and5_IDHotel_name"));
+        us_011_hotelRoomSearchPositivePage.codeTextBox.sendKeys(ConfigReader.getProperty("tc4and5_code_textbox"));
+        us_011_hotelRoomSearchPositivePage.nameTextBox.sendKeys(ConfigReader.getProperty("tc4and5_name_textbox"));
+        us_011_hotelRoomSearchPositivePage.locationTextBox.sendKeys(ConfigReader.getProperty("tc4and5_location_textbox"));
+        us_011_hotelRoomSearchPositivePage.priceTextBox.sendKeys(ConfigReader.getProperty("tc4and5_price_textbox"));
+        Select select1 = new Select(us_011_hotelRoomSearchPositivePage.isAvalaibleDropdown);
+        select1.selectByVisibleText("True");
+        us_011_hotelRoomSearchPositivePage.clearButton.click();
+        Assert.assertEquals(us_011_hotelRoomSearchPositivePage.idTextBox.getText(),ConfigReader.getProperty("clear_dorulama"));
 
     }
 }
--- src/test/java/tests/US_012_Negative.java
+++ src/test/java/tests/US_012_Negative.java
@@ -1,4 +1,77 @@
 package tests;
 
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.US_011_012_HotelRoomSearchPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class US_012_Negative {
+    US_011_012_HotelRoomSearchPage us_012_hotelRoomSearchNegativePage = new US_011_012_HotelRoomSearchPage();
+
+    public void login () {
+        Driver.getDriver().get(ConfigReader.getProperty("p_url"));
+
+        us_012_hotelRoomSearchNegativePage.loginButonu.click();
+        us_012_hotelRoomSearchNegativePage.userNameTextBox.sendKeys(ConfigReader.getProperty("valid_user"));
+        us_012_hotelRoomSearchNegativePage.passwordTextBox.sendKeys(ConfigReader.getProperty("valid_password"));
+        us_012_hotelRoomSearchNegativePage.girisButtonu.click();
+        us_012_hotelRoomSearchNegativePage.hotelManagementLinki.click();
+        us_012_hotelRoomSearchNegativePage.hotelRoomsLinki.click();
+
+    }
+    @Test
+    public void tc_006(){
+        login();
+        us_012_hotelRoomSearchNegativePage.clearButton.click();
+        us_012_hotelRoomSearchNegativePage.idTextBox.sendKeys(ConfigReader.getProperty("tc6_ID_textbox_invalid"));
+        us_012_hotelRoomSearchNegativePage.searchButton.click();
+        Assert.assertTrue(us_012_hotelRoomSearchNegativePage.bulunamadiYazisi.isDisplayed());
+
+    }
+    @Test
+    public void tc_007(){
+        login();
+        us_012_hotelRoomSearchNegativePage.clearButton.click();
+        us_012_hotelRoomSearchNegativePage.codeTextBox.sendKeys(ConfigReader.getProperty("tc7and10_code_textbox_invalid"));
+        us_012_hotelRoomSearchNegativePage.searchButton.click();
+        Assert.assertTrue(us_012_hotelRoomSearchNegativePage.aradiginDataYok.isDisplayed());
+    }
+    @Test
+    public void tc_008(){
+        login();
+        us_012_hotelRoomSearchNegativePage.clearButton.click();
+        us_012_hotelRoomSearchNegativePage.nameTextBox.sendKeys(ConfigReader.getProperty("tc8_name_textbox_invalid"));
+        us_012_hotelRoomSearchNegativePage.searchButton.click();
+        Assert.assertTrue(us_012_hotelRoomSearchNegativePage.bulunamadiYazisi.isDisplayed());
+    }
+    @Test
+    public void tc_009(){
+        login();
+        us_012_hotelRoomSearchNegativePage.clearButton.click();
+        us_012_hotelRoomSearchNegativePage.locationTextBox.sendKeys(ConfigReader.getProperty("tc9and10_location_textbox_invalid"));
+        us_012_hotelRoomSearchNegativePage.searchButton.click();
+        Assert.assertTrue(us_012_hotelRoomSearchNegativePage.bulunamadiYazisi.isDisplayed());
+    }
+    @Test
+    public void tc_010(){
+        login();
+        us_012_hotelRoomSearchNegativePage.idTextBox.sendKeys(ConfigReader.getProperty("tc3and10_ID_textbox"));
+        us_012_hotelRoomSearchNegativePage.codeTextBox.sendKeys(ConfigReader.getProperty("tc7and10_code_textbox_invalid"));
+        us_012_hotelRoomSearchNegativePage.nameTextBox.sendKeys(ConfigReader.getProperty("tc10_name_textbox_invalid"));
+        us_012_hotelRoomSearchNegativePage.locationTextBox.sendKeys(ConfigReader.getProperty("tc9and10_location_textbox_invalid"));
+        us_012_hotelRoomSearchNegativePage.priceTextBox.sendKeys(ConfigReader.getProperty("tc3and10_price_textbox"));
+        Select select=new Select(us_012_hotelRoomSearchNegativePage.isAvalaibleDropdown);
+        select.selectByVisibleText("False");
+        us_012_hotelRoomSearchNegativePage.searchButton.click();
+        Assert.assertTrue(us_012_hotelRoomSearchNegativePage.bulunamadiYazisi.isDisplayed());
+
+
+
+
+    }
+
 }
