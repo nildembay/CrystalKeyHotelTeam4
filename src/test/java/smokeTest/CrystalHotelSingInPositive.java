e03a4f79f987c5587316e8285e213d2606af11ff
nildembay <nildembay@gmail.com>
2020-12-31 15:24:02 +0300

    enson

--- src/test/java/smokeTest/CrystalHotelSingInPositive.java
+++ src/test/java/smokeTest/CrystalHotelSingInPositive.java
@@ -7,18 +7,25 @@ import pages.CrystalHotelSignIn;
 import utilities.ConfigReader;
 import utilities.Driver;
 import org.testng.Assert;
+import utilities.TestBaseRapor;
 
-public class CrystalHotelSingInPositive {
+public class CrystalHotelSingInPositive extends TestBaseRapor {
     @Test
     public void SignIn(){
+        extentTest= extentReports.createTest("CrystalKey SignIn PositiveTest","geçerli veriler ile sayfaya giriş sağlanabilmeli");
         Driver.getDriver().get(ConfigReader.getProperty("p_url"));
         CrystalHotelSignIn crystalHotelSignIn=new CrystalHotelSignIn();
+
         crystalHotelSignIn.loginButonu.click();
-        crystalHotelSignIn.userNameTextBox.sendKeys(ConfigReader.getProperty("val   id_user"));
+        extentTest.info("login butonuna tıklanıp, id-sifre sayfasına gelindi.");
+        crystalHotelSignIn.userNameTextBox.sendKeys(ConfigReader.getProperty("valid_user"));
         crystalHotelSignIn.passwordTextBox.sendKeys(ConfigReader.getProperty("valid_password"));
         crystalHotelSignIn.girisButton.click();
+        extentTest.info("geçerli bilgiler girildi.");
         Assert.assertTrue(crystalHotelSignIn.managerButton.isDisplayed());
+        extentTest.pass("Geçerli bilgiler ile başarılı bir şekilde giriş saglandı.");
         Driver.closeDriver();
+
 
 
     }
